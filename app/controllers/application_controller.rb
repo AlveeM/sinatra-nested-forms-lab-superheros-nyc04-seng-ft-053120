be82fd537d2984486cbe9720b28cf483f10360a6
AlveeM <alvee.akand@outlook.com>
2020-07-13 08:23:28 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
+
+    post '/teams' do
+        team = params[:team]
+        @team_name = team[:name]
+        @team_motto = team[:motto]
+        @heroes = team[:members]
+
+        erb :team
+    end
 
 end
